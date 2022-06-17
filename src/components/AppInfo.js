aaf0286a23d457d984533b15250db6c91e2844a0
nerajno <nerajno@gmail.com>
2022-06-16 22:29:33 -0400

    added linting

--- src/components/AppInfo.js
+++ src/components/AppInfo.js
@@ -2,15 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import BooksContext from '../context/BooksContext';
 
-
-const BookInfo = () => {
-return (
-    <div className="bookinfo">
-        <h2>What is Calib ?</h2>
-        <p>Basic Info and Additions</p>
-        <p> Tech Specs</p>
+const AppInfo = () => {
+  return (
+    <div className='AppInfo'>
+      <h2> What is Calib ? </h2> <p> Basic Info and Additions </p>{' '}
+      <p> Tech Specs </p>{' '}
     </div>
-)
-}
+  );
+};
 
-export default BookInfo;
+export default AppInfo;
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,34 +1,63 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
 import Header from '../components/Header';
 import AddBook from '../components/AddBook';
 import BooksList from '../components/BooksList';
 import useLocalStorage from '../hooks/useLocalStorage';
 import EditBook from '../components/EditBook';
 import BooksContext from '../context/BooksContext';
-import BookInfo from '../components/AppInfo';
+import AppInfo from '../components/AppInfo';
 
 const AppRouter = () => {
   const [books, setBooks] = useLocalStorage('books', []);
 
 
-  return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <div className="main-content">
-          <BooksContext.Provider value={{ books, setBooks }}>
-            <Switch>
-              {/* <Route component={BookInfo} path="/" /> */}
-              <Route component={BooksList} path="/" exact={true} />
-              <Route component={AddBook} path="/add" />
-              <Route component={EditBook} path="/edit/:id" />
-              {/* <Route component={() => <Redirect to="/" />} /> */}
-            </Switch>
-          </BooksContext.Provider>
-        </div>
-      </div>
-    </BrowserRouter>
+  return ( <
+    BrowserRouter >
+    <
+    div >
+    <
+    Header / >
+    <
+    div className = "main-content" >
+    <
+    BooksContext.Provider value = {
+      {
+        books,
+        setBooks
+      }
+    } >
+    <
+    Switch > {
+      /* <Route component={AppInfo} path="/" /> */ } <
+    Route component = {
+      BooksList
+    }
+    path = "/"
+    exact = {
+      true
+    }
+    /> <
+    Route component = {
+      AddBook
+    }
+    path = "/add" / >
+    <
+    Route component = {
+      EditBook
+    }
+    path = "/edit/:id" / > {
+      /* <Route component={() => <Redirect to="/" />} /> */ } <
+    /Switch> <
+    /BooksContext.Provider> <
+    /div> <
+    /div> <
+    /BrowserRouter>
   );
 };
 
